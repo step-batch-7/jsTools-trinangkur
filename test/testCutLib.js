a9edb474be612ee44b247d36c6fdc3c66ff67286
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2019-12-21 18:14:55 +0530

    Added test in getFieldsToDisplay
    testing for a given empty list it should return an empty string...

--- test/testCutLib.js
+++ test/testCutLib.js
@@ -6,4 +6,8 @@ describe("getFieldsToDisplay", function() {
     const actual = getFieldsToDisplay([1, 2, 3]);
     assert.strictEqual(actual, "1\n2\n3");
   });
+  it("for a given empty list should return empty stirng", function() {
+    const actual = getFieldsToDisplay([]);
+    assert.strictEqual(actual, "");
+  });
 });
