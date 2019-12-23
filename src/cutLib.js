cce9eff19f2c7691c4cf38b32adb042949910eba
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2019-12-23 10:17:38 +0530

    Added new function getLines
    for a givne chunk it will return an array having vlaues of that
    seperated by new line...

--- src/cutLib.js
+++ src/cutLib.js
@@ -2,4 +2,8 @@ const formatFields = function(list) {
   return list.join("\n");
 };
 
-module.exports = { formatFields };
+const getLines = function(chunk) {
+  return chunk.split("\n");
+};
+
+module.exports = { formatFields, getLines };
--- test/testCutLib.js
+++ test/testCutLib.js
@@ -1,5 +1,5 @@
-const { formatFields } = require("../src/cutLib");
-const assert = require("assert");
+const { formatFields, getLines } = require("../src/cutLib");
+const assert = require("chai").assert;
 
 describe("formatFields", function() {
   it("for a given list should join them with new line", function() {
@@ -9,5 +9,16 @@ describe("formatFields", function() {
   it("for a given empty list should return empty stirng", function() {
     const actual = formatFields([]);
     assert.strictEqual(actual, "");
+  });
+});
+
+describe("getLines", function() {
+  it("should get every fileds seperated by new line", function() {
+    const actual = getLines("abc\ncd");
+    assert.deepStrictEqual(actual, ["abc", "cd"]);
+  });
+  it("should return whole chunk in one array if the no new line is there", function() {
+    const actual = getLines("abcd");
+    assert.deepStrictEqual(actual, ["abcd"]);
   });
 });
