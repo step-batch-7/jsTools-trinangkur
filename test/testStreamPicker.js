e7c33ac156895554ab16c5fe17ebc7b5853b3b84
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2020-01-08 14:45:55 +0530

    added test

--- test/testStreamPicker.js
+++ test/testStreamPicker.js
@@ -2,7 +2,7 @@ const StreamPicker = require('../src/streamPicker');
 const { assert } = require('chai');
 
 describe('StreamPicker', function() {
-  it.only('should get readStream for a given path', function() {
+  it('should get readStream for a given path', function() {
     const readStream = {};
     const creatReadStream = path => {
       assert.strictEqual(path, 'givenPath');
@@ -12,7 +12,7 @@ describe('StreamPicker', function() {
     const streamPicker = new StreamPicker(creatReadStream, rl);
     assert.strictEqual(streamPicker.pick('givenPath'), readStream);
   });
-  it.only('should get rl for not given path', function() {
+  it('should get rl for not given path', function() {
     const readStream = {};
     const creatReadStream = path => {
       assert.strictEqual(path, 'givenPath');
